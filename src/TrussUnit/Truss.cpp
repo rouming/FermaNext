e2c7bd4792ce8cee4d6e12da9aff4e20e8b5d56e
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2005-04-25 17:01:25 +0000

    Minor tweaks; Add Qt signals emitting to notice object status changings; All code should be tested a lot; New 'moc_' file in VS project;
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk/FermaNext@31 d51c77f7-620d-0410-a109-9a2f07eaa567

--- src/TrussUnit/Truss.cpp
+++ src/TrussUnit/Truss.cpp
@@ -7,20 +7,25 @@
  *****************************************************************************/
 
 Node::Node () : 
-    x(0), y(0), fix(FixationLack)
+    x(0), y(0), fix(Unfixed)
 {}
 
 Node::Node ( int x_, int y_ ) :
-    x(x_), y(y_), fix(FixationLack)
+    x(x_), y(y_), fix(Unfixed)
 {}
 
 Node::Node ( int x_, int y_, Fixation fix_ ) :
     x(x_), y(y_), fix(fix_)
 {}
 
+Node::Node ( const Node& n ) :
+    x(n.x), y(n.y), fix(n.fix)
+{}
+
 void Node::setFixation ( Node::Fixation fix_ )
 {
     fix = fix_;
+    emit onFixationChange(fix);
 }
 
 Node::Fixation Node::getFixation () const
@@ -28,112 +33,17 @@ Node::Fixation Node::getFixation () const
     return fix;
 }
 
-/*****************************************************************************
- * Pivot
- *****************************************************************************/
-/*
-Pivot<class Node>::Pivot () :
-    first(NULL),
-    last(NULL)
-{}
-
-Pivot<class Node>::Pivot ( const Node& first_, const Node& last_ ) :
-    first(&first_),
-    last(&last_)
-{}
-
-void Pivot<class Node>::setFirstNode ( const Node& first_ )
+void Node::loadState ( const Node& n ) 
 {
-    first = &first_;
+    emit beforeStateLoaded();    
+    x = n.x;
+    y = n.y;    
+    emit onPositionChange(x, y);
+    fix = n.fix;
+    emit onFixationChange(fix);
+    emit afterStateLoaded();
 }
 
-const Node& Pivot<class Node>::getFirstNode () const
-{
-    return *first;
-}
-
-void Pivot<class Node>::setLastNode ( const Node& last_ )
-{
-    last = &last_;
-}
-
-const Node& Pivot<class Node>::getLastNode () const
-{
-    return *last;
-}
-*/
-
-/*****************************************************************************
- * Truss
- *****************************************************************************/
-/*
-template <class Node, class Pivot>
-Truss<Node, Pivot>::Truss ()
-{}
-
-template <class Node, class Pivot>
-Truss<Node, Pivot>::~Truss ()
-{
-    NodeList::iterator itNodes;
-    PivotList::iterator itPivots;
-    for ( itNodes = nodes.begin(); itNodes != nodes.end();  ++itNodes )
-        delete *itNodes;
-    for ( itPivots = pivots.begin(); itPivots != pivots.end();  ++itPivots )
-        delete *itPivots;
-}
-
-template <class Node, class Pivot>
-Node& Truss<Node, Pivot>::createNode ()
-{
-    Node* node = new Node;
-    nodes.push_back(node);
-    return *node;
-}
-
-template <class Node, class Pivot>
-Pivot& Truss<Node, Pivot>::createPivot ()
-{
-    Pivot* pivot = new Pivot;
-    pivots.push_back(pivot);
-    return *pivot;
-}
-
-template <class Node, class Pivot>
-Pivot& Truss<class Node, class Pivot>::createPivot ( const Node& first, 
-                                                     const Node& last )
-{
-    Pivot* pivot = new Pivot( first, last );
-    pivots.push_back(pivot);
-    return *pivot;    
-}
-
-template <class Node, class Pivot>
-bool Truss<class Node, class Pivot>::removePivot ( const Pivot& pivot )
-{
-    PivotList::iterator iter = pivots.begin();
-    for ( ; iter != pivots.end(); ++iter )
-        if ( (*iter) == &pivot ) {
-            delete *iter;
-            pivots.erase(iter);
-            return true;
-        }            
-    return false;    
-}
-
-template <class Node, class Pivot>
-bool Truss<class Node, class Pivot>::removeNode ( const Node& node )
-{
-    NodeList::iterator iter = nodes.begin();
-    for ( ; iter != nodes.end(); ++iter )
-        if ( (*iter) == &node ) {
-            delete *iter;
-            nodes.erase(iter);
-            return true;
-        }            
-    return false;    
-}
-
- */
 /****************************************************************************/
 
 
