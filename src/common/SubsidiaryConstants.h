88dfd99cda4cb2b8025c78a0bfcb5945a55a780e
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2005-05-14 10:11:47 +0000

    Add config in XML format and class 'FermaNextConfig' for managing this config (config test is added too); Add sources for all subsidiary constans
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk/FermaNext@58 d51c77f7-620d-0410-a109-9a2f07eaa567

---
+++ src/common/SubsidiaryConstants.h
@@ -0,0 +1,23 @@
+
+#ifndef SUBSIDIARYCONSTANTS_H
+#define SUBSIDIARYCONSTANTS_H
+
+#include <qstring.h>
+
+/************************************
+ * Main Consts 
+ ************************************/
+
+extern const QString imagesPath;
+
+
+/************************************
+ * Truss Unit Consts
+ ************************************/
+
+
+/************************************
+ * Desginer Consts
+ ************************************/
+
+#endif //SUBSIDIARYCONSTANTS_H
