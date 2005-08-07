a03d3e181e24001ea89b7e39a99b0f7ce38ff549
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2005-08-07 17:54:46 +0000

    Undo/Redo on create/remove truss unit objects; suspended clean conception; fix bug with node number
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk/FermaNext@154 d51c77f7-620d-0410-a109-9a2f07eaa567

---
+++ src/TrussUnit/TrussUnitActions.h
@@ -0,0 +1,114 @@
+
+#ifndef TRUSSUNITACTIONS_H
+#define TRUSSUNITACTIONS_H
+
+#include "AbstractObjectAction.h"
+#include "TrussUnit.h"
+#include "TrussUnitWindowManager.h"
+
+/*****************************************************************************
+ * Truss Node Actions
+ *****************************************************************************/
+
+class TrussNodeAction : public AbstractObjectAction
+{
+public:
+    TrussNodeAction ( TrussUnit&, TrussNode& );
+
+    TrussUnit& getTruss ();
+    TrussNode& getNode ();
+
+private:
+    TrussUnit& truss;
+    TrussNode& node;
+};
+
+class TrussNodeCreateAction : public TrussNodeAction
+{
+public:
+    TrussNodeCreateAction ( TrussUnit&, TrussNode& );
+
+    void execute ();
+    void unexecute ();
+};
+
+class TrussNodeRemoveAction : public TrussNodeAction
+{
+public:
+    TrussNodeRemoveAction ( TrussUnit&, TrussNode& );
+
+    void execute ();
+    void unexecute ();
+};
+
+/*****************************************************************************
+ * Truss Pivot Actions
+ *****************************************************************************/
+
+class TrussPivotAction : public AbstractObjectAction
+{
+public:
+    TrussPivotAction ( TrussUnit&, TrussPivot& );
+
+    TrussUnit& getTruss ();
+    TrussPivot& getPivot ();
+
+private:
+    TrussUnit& truss;
+    TrussPivot& pivot;
+};
+
+class TrussPivotCreateAction : public TrussPivotAction
+{
+public:
+    TrussPivotCreateAction ( TrussUnit&, TrussPivot& );
+
+    void execute ();
+    void unexecute ();
+};
+
+class TrussPivotRemoveAction : public TrussPivotAction
+{
+public:
+    TrussPivotRemoveAction ( TrussUnit&, TrussPivot& );
+
+    void execute ();
+    void unexecute ();
+};
+
+/*****************************************************************************
+ * Truss Unit Window Actions
+ *****************************************************************************/
+
+class TrussUnitWindowAction : public AbstractObjectAction
+{
+public:
+    TrussUnitWindowAction ( TrussUnitWindowManager&, TrussUnitWindow& );
+
+    TrussUnitWindow& getTruss ();
+    TrussUnitWindowManager& getTrussManager ();
+
+private:
+    TrussUnitWindowManager& mng;
+    TrussUnitWindow& truss;
+};
+
+class TrussUnitWindowCreateAction : public TrussUnitWindowAction
+{
+public:
+    TrussUnitWindowCreateAction ( TrussUnitWindowManager&, TrussUnitWindow& );
+
+    void execute ();
+    void unexecute ();
+};
+
+class TrussUnitWindowRemoveAction : public TrussUnitWindowAction
+{
+public:
+    TrussUnitWindowRemoveAction ( TrussUnitWindowManager&, TrussUnitWindow& );
+
+    void execute ();
+    void unexecute ();
+};
+
+#endif //TRUSSUNITACTIONS_H
