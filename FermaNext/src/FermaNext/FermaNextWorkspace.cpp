bcd42806b2661179455e1849a65ebaca305d6902
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2005-09-03 15:02:46 +0000

    File was moved here from 'common' directory
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk@255 d51c77f7-620d-0410-a109-9a2f07eaa567

---
+++ FermaNext/src/FermaNext/FermaNextWorkspace.cpp
@@ -0,0 +1,163 @@
+
+#include "FermaNextWorkspace.h"
+#include "FermaNextMainFrame.h"
+
+#include <qapplication.h>
+#include <qwidgetstack.h>
+
+const QString untitledName( QObject::tr("Unnamed workspace") );
+
+/*****************************************************************************
+ * FermaNext Workspace
+ *****************************************************************************/
+
+FermaNextWorkspace* FermaNextWorkspace::instance = 0;
+QMutex FermaNextWorkspace::mutex;
+
+FermaNextWorkspace::FermaNextWorkspace () :
+    name(untitledName),
+    widgetStack(0)
+{
+    // Singleton desist by Qt quit signal
+    connect( qApp, SIGNAL(lastWindowClosed()), SLOT(clear()) );
+}
+
+FermaNextWorkspace::~FermaNextWorkspace ()
+{
+    clear();
+}
+
+FermaNextWorkspace& FermaNextWorkspace::workspace ()
+{
+    if ( instance == 0 ) {
+        mutex.lock();
+        if ( instance == 0 ) 
+            instance = new FermaNextWorkspace;
+        mutex.unlock();
+    }
+    return *instance;
+}
+
+void FermaNextWorkspace::clear ()
+{
+    ProjectListIter iter = projects.begin();
+    for ( ; iter != projects.end(); ++iter )       
+      delete *iter;    
+    projects.clear();
+}
+
+void FermaNextWorkspace::reset ()
+{
+    emit onReset();
+    name = untitledName;    
+    clear();    
+}
+
+void FermaNextWorkspace::createWidgetStack ( QMainWindow& parent )
+{
+    if ( widgetStack )
+        return;
+    widgetStack = new QWidgetStack(&parent);
+    parent.setCentralWidget(widgetStack);
+}
+
+FermaNextProject& FermaNextWorkspace::createProject ( const QString& name )
+                                     throw (WorkspaceIsNotInitedCorrectly)
+{
+    // Assuming widget stack was created.
+    if ( widgetStack == 0 )
+        throw WorkspaceIsNotInitedCorrectly();
+    FermaNextProject* project = new FermaNextProject(name, widgetStack);
+    projects.push_back(project);
+    emit onProjectCreate(*project);    
+    return *project;
+}
+
+bool FermaNextWorkspace::removeProject ( FermaNextProject& project )
+{    
+    ProjectListIter iter = projects.begin();
+    for ( ; iter != projects.end(); ++iter ) 
+        if ( (*iter) == &project ) {  
+            FermaNextProject* prj = *iter;
+            emit onProjectRemove(*prj);
+            projects.erase(iter);
+            delete prj;
+            return true;
+    }
+    return false; 
+}
+
+bool FermaNextWorkspace::removeProject ( const QString& name )
+{ 
+    ProjectListIter iter = projects.begin();
+    for ( ; iter != projects.end(); ++iter ) 
+        if ( (*iter)->getName() == name ) {  
+            FermaNextProject* prj = *iter;
+            emit onProjectRemove(*prj);
+            projects.erase(iter);
+            delete prj;
+            return true;
+    }
+    return false; 
+}
+
+void FermaNextWorkspace::activateProject ( const QString& prjName )
+{
+    FermaNextProject* prj = findProjectByName(prjName);
+    if ( prj )
+        prj->activate();
+}
+
+FermaNextProject* FermaNextWorkspace::findProjectByName ( const QString& name )
+{
+    ProjectListIter iter = projects.begin();
+    for ( ; iter != projects.end(); ++iter ) 
+        if ( (*iter)->getName() == name )
+            return *iter;    
+    return 0;
+}
+
+int FermaNextWorkspace::countProjects () const
+{
+    return (int)projects.size();
+}
+
+FermaNextProject& FermaNextWorkspace::getProject ( int index )
+{
+    FermaNextProject* prj = projects.at((size_t)index);
+    return *prj;
+}
+
+const QString& FermaNextWorkspace::getName () const
+{
+    return name;
+}
+
+void FermaNextWorkspace::setName ( const QString& name_ )
+{
+    name = name_;
+    emit onNameChange(name);
+}
+
+FermaNextConfig& FermaNextWorkspace::config ()
+{
+    return fermaConfig;
+}
+
+bool FermaNextWorkspace::loadPlugins ()
+{
+    FermaNextConfig& conf = config();
+    ConfigItems items = conf.getConfigItems("PluginsDir");
+    if ( ! items.contains("PluginsDir") )
+        return false;
+    QString pluginsDir = items["PluginsDir"].toString();
+    plgLoader.loadPlugins( pluginsDir );
+    return true;
+}
+
+PluginLoader& FermaNextWorkspace::pluginLoader ()
+{
+    return plgLoader;
+}
+
+/*****************************************************************************/
