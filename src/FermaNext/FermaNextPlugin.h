946e30f47093ef1bcaf531bd30b233898b88bd4e
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2005-09-04 19:53:26 +0000

    Implement DSO API for calling constructor/destructor
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk/FermaNext@261 d51c77f7-620d-0410-a109-9a2f07eaa567

--- src/FermaNext/FermaNextPlugin.h
+++ src/FermaNext/FermaNextPlugin.h
@@ -19,6 +19,8 @@
 enum PluginType { CALCULATION_PLUGIN = 0, 
                   OPTIMIZATION_PLUGIN };
 
+// Plain Old Data for plugin info.
+// Plugin name should be short and unique.
 struct PluginInfo
 {
     const char* name;
@@ -26,8 +28,72 @@ struct PluginInfo
     PluginType type;
 };
 
-// Plugin signature. Plugin name should be short and unique.
-const char PLUGIN_INFO_CALL[] = "ferma_next_plugin_info";
+
+#define PLUGIN_INFO ferma_next_plugin_info
+#define PLUGIN_INFO_CALL "ferma_next_plugin_info"
+
+
+// Plugin signature. 
 typedef PluginInfo& (* PluginInfoCall ) ();
+
+#if defined WINDOWS || defined WIN32
+  #include <windows.h>
+
+  #define FERMA_NEXT_PLUGIN_INIT \
+    void init_plugin__ (); \
+    class caller_init__ \
+    { public: caller_init__() { initCaller__ = init_plugin__; } }; \
+    caller_init__ init__; \
+    void init_plugin__ ()
+
+  #define FERMA_NEXT_PLUGIN_FINI \
+    void fini_plugin__ (); \
+    class caller_fini__ \
+    { public: caller_fini__() { finiCaller__ = fini_plugin__; } }; \
+    caller_fini__ fini__; \
+    void fini_plugin__ ()
+
+  #define FERMA_NEXT_PLUGIN(info) \
+    PluginExport PluginInfo& PLUGIN_INFO () \
+    { return info; } \
+    \
+    typedef void (* InitFiniCall__) (); \
+    template <typename Call> \
+    class caller__ \
+    { \
+    public: \
+      caller__ () : call(0) {} \
+      caller__& operator= ( const Call& call_ ) \
+      { call = call_; return *this; } \
+      void operator() () { if ( call ) call(); } \
+    private: \
+      Call call; \
+    }; \
+    caller__<InitFiniCall__> initCaller__; \
+    caller__<InitFiniCall__> finiCaller__; \
+    \
+    BOOL WINAPI DllMain( \
+      HINSTANCE hinstDLL, \
+      DWORD fdwReason,    \
+      LPVOID lpvReserved  \
+    ) \
+    { \
+      if ( fdwReason == DLL_PROCESS_ATTACH ) \
+        initCaller__(); \
+      else if ( fdwReason == DLL_PROCESS_DETACH ) \
+        finiCaller__(); \
+      return TRUE; \
+    }
+#else
+  #define FERMA_NEXT_PLUGIN_INIT \
+    __attribute__((constructor)) void init_plugin__ ()
+  #define FERMA_NEXT_PLUGIN_FINI \
+    __attribute__((destructor)) void fini_plugin__ ()
+
+  #define FERMA_NEXT_PLUGIN(info) \
+    PluginExport PluginInfo& PLUGIN_INFO () \
+    { return info; }
+#endif
+
 
 #endif //FERMANEXTPLUGIN_H
