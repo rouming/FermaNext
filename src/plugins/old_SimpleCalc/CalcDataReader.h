9cc3aaac54008d3358bdb3a724474a10542486c7
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2005-09-06 10:34:03 +0000

    Convert to win-1251 coding system
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk/FermaNext@270 d51c77f7-620d-0410-a109-9a2f07eaa567

--- src/plugins/old_SimpleCalc/CalcDataReader.h
+++ src/plugins/old_SimpleCalc/CalcDataReader.h
@@ -7,22 +7,22 @@
 
 struct CalcData
 {
-    int NumNods;                      //ЊЪЊЮЊЫЊШЊзЊХЊбЊвЊТЊЮ ЊГЊЧЊЫЊЮЊТ
-    int NumPivots;                    //ЊЪЊЮЊЫЊШЊзЊХЊбЊвЊТЊЮ ЊБЊвЊХЊаЊЦЊЭЊХЊЩ
-    int NumLoads;                     //ЊЪЊЮЊЫЊШЊзЊХЊбЊвЊТЊЮ ЊБЊЫЊгЊзЊРЊХЊТ ЊЭЊРЊУЊаЊгЊЦЊХЊЭЊШЊп 
-    double StressLimit;               //ЊґЊЮЊЯЊгЊбЊЪЊРЊХЊЬЊЮЊХ ЊЭЊРЊЯЊаЊпЊЦЊХЊЭЊШЊХ
-    QMemArray <int> PivotsFirstNod;   //ЊЬЊРЊбЊбЊШЊТ ЊЯЊХЊаЊТЊлЊе ЊЭЊЮЊЬЊХЊаЊЮЊТ ЊГЊЧЊЫЊЮЊТ ЊТ ЊБЊвЊХЊаЊЦЊЭЊпЊе
-    QMemArray <int> PivotsSecondNod;  // "__" ЊТЊвЊЮЊаЊлЊе "__"
-    QMemArray <double> X;             //ЊЬЊРЊбЊбЊШЊТ X ЊЪЊЮЊЮЊаЊФЊШЊЭЊРЊв ЊГЊЧЊЫЊЮЊТ
+    int NumNods;                      //количество Узлов
+    int NumPivots;                    //количество Стержней
+    int NumLoads;                     //количество Случаев нагружения 
+    double StressLimit;               //Допускаемое напряжение
+    QMemArray <int> PivotsFirstNod;   //массив первых номеров Узлов в Стержнях
+    QMemArray <int> PivotsSecondNod;  // "__" вторых "__"
+    QMemArray <double> X;             //массив X координат Узлов
     QMemArray <double> Y;             //"__" Y "__"
-    QMemArray <double> TransX;        //ЊЬЊРЊбЊбЊШЊТ ЊЯЊХЊаЊХЊЬЊХЊйЊХЊЭЊШЊЩ ЊЯЊЮ X
-    QMemArray <double> TransY;        //ЊЬЊРЊбЊбЊШЊТ ЊЯЊХЊаЊХЊЬЊХЊйЊХЊЭЊШЊЩ ЊЯЊЮ Y
-    QMemArray <double> Stress;        //ЊЭЊРЊЯЊаЊпЊЦЊХЊЭЊШЊп ЊТ ЊБЊвЊХЊаЊЦЊЭЊпЊе
-    QMemArray <double> StressReserve; //ЊЧЊРЊЯЊРЊб ЊїЊаЊЮЊзЊЭЊЮЊбЊвЊШ
-    QMemArray <double> S;             //ЊЯЊЮЊвЊаЊХЊСЊЭЊлЊХ ЊЯЊЫЊЮЊйЊРЊФЊШ ЊБЊвЊХЊаЊЦЊЭЊХЊЩ
-    double V;                         //ЊѕЊСЊкЊсЊЬ
-    double Sg;                        //ЊБЊШЊЫЊЮЊТЊЮЊЩ ЊТЊХЊб ЊЪЊЮЊЭЊбЊвЊаЊгЊЪЊжЊШЊШ
-    QMemArray <double> PivotLength;   //ЊФЊЫЊШЊЭЊР ЊБЊвЊХЊаЊЦЊЭЊп
+    QMemArray <double> TransX;        //массив перемещений по X
+    QMemArray <double> TransY;        //массив перемещений по Y
+    QMemArray <double> Stress;        //напряжения в Стержнях
+    QMemArray <double> StressReserve; //запас Прочности
+    QMemArray <double> S;             //потребные площади Стержней
+    double V;                         //Объём
+    double Sg;                        //Силовой вес конструкции
+    QMemArray <double> PivotLength;   //длина Стержня
 };
 
 class CalcDataReader
