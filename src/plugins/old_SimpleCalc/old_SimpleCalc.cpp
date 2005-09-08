c4af0539d080db00befcff57f1f56e3255f332fb
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2005-09-08 14:46:14 +0000

    Linux implementation of calculation server startup
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk/FermaNext@276 d51c77f7-620d-0410-a109-9a2f07eaa567

--- src/plugins/old_SimpleCalc/old_SimpleCalc.cpp
+++ src/plugins/old_SimpleCalc/old_SimpleCalc.cpp
@@ -2,6 +2,7 @@
 #include "old_SimpleCalc.h"
 #include "FermaNextPlugin.h"
 #include <stdio.h>
+#include <qsocket.h>
 #include <qfile.h>
 
 /*****************************************************************************
@@ -14,11 +15,20 @@ FERMA_NEXT_PLUGIN(inf)
 FERMA_NEXT_PLUGIN_INIT
 {
     tempFile = createTempFile();
+    tempSocket = startCalculationServer();
 }
 
 FERMA_NEXT_PLUGIN_FINI
 {
+    stopCalculationServer( tempSocket );
     destroyTempFile( tempFile );
+}
+
+PluginExport void StandardCall calculation ()
+{
+    // write to calculation server
+    QTextStream os(tempSocket);
+    os << "/home/roman/devel/FermaNext/1-64578.frm" << "\n";    
 }
 
 /*****************************************************************************/
@@ -41,6 +51,26 @@ void destroyTempFile ( QFile*& file )
         delete file;
         file = 0;        
     }
+}
+
+QSocket* startCalculationServer ()
+{
+    CalcThread* calc = new CalcThread;
+    calc->start();
+    sleep_seconds(3);
+    QSocket* socket = new QSocket();
+    socket->connectToHost( "localhost", 1212 );
+    return socket;
+}
+
+void stopCalculationServer ( QSocket*& socket )
+{
+    QTextStream os(socket);
+    os << "quit";
+    socket->close();
+    sleep_seconds(3);
+    delete socket;
+    socket = 0;
 }
 
 /*****************************************************************************/
--- src/plugins/old_SimpleCalc/old_SimpleCalc.h
+++ src/plugins/old_SimpleCalc/old_SimpleCalc.h
@@ -2,16 +2,50 @@
 #ifndef OLD_SIMPLECALC_H
 #define OLD_SIMPLECALC_H
 
+#include <qthread.h>
+
+#if defined WINDOWS || defined WIN32  
+  #include <windows.h>
+  #define sleep_seconds(secs) Sleep(secs*1000)
+#else
+  #include <unistd.h>
+  #define sleep_seconds(secs) sleep(secs)
+#endif
+
 class QFile;
+class QSocket;
 
 // Temp file
 QFile* tempFile = 0;
+// Temp socket
+QSocket* tempSocket = 0;
 
 const char* fermaResExt = ".vyv";
 
 // Manages temp file
 QFile* createTempFile ();
 void destroyTempFile ( QFile*& );
+
+// Manages calculation server
+QSocket* startCalculationServer ();
+void stopCalculationServer ( QSocket*& );
+
+
+// Calculation thread
+class CalcThread : public QThread 
+{
+public:
+    virtual void run()
+    {
+#if defined WINDOWS || defined WIN32  
+        system("plugins/Simple_f.exe 1212");
+#else
+        // Wine call. Assuming it is installed
+        system("wine plugins/Simple_f.exe 1212");
+#endif
+    }
+};
+
 
 
 #endif //OLD_SIMPLECALC_H
