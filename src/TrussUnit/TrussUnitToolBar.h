86f5182f4acf3943ff10a41f0dca75884e092fdf
enarion <enarion@d51c77f7-620d-0410-a109-9a2f07eaa567>
2005-09-18 12:39:49 +0000

    Add forgotten files
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk/FermaNext@305 d51c77f7-620d-0410-a109-9a2f07eaa567

--- src/TrussUnit/TrussUnitToolBar.h
+++ src/TrussUnit/TrussUnitToolBar.h
@@ -3,15 +3,38 @@
 #define TRUSSUNITTOOLBAR_H
 
 #include "AggToolBar.h"
+#include "qtimer.h"
+
+/*****************************************************************************/
+
+class AggToolBarHideButton : public AggButton
+{
+public:
+    AggToolBarHideButton ( QPoint leftTopPos, int width, int height );
+    virtual ~AggToolBarHideButton ();
+    virtual void paint ( ren_dynarow& baseRend, scanline_rasterizer& ras,
+                         agg::scanline_p8& sl, solidRenderer& solidRend  ) const;
+private:
+    color_type fillCol, lineCol, highlightFill;
+};
+
+/*****************************************************************************/
 
 class TrussUnitToolBar : public AggToolBar
 {
+    Q_OBJECT
 public:
     TrussUnitToolBar ( QPoint pos, int bordLeft, int bordRight, int bordTop, 
                        int bordBottom, int separation, int rad );
     ~TrussUnitToolBar ();
 
+    void addHideButton ( QPoint leftTopPos, int width, int height, QObject* widget );
+    AggToolBarHideButton* getHideButton () const;
+    virtual void removeHideButton ();
+
+    virtual QPoint getDynarowBufPos ( int x, int y ) const;
     virtual bool inToolBarRect ( int x, int y ) const;
+
     virtual void checkMouseMoveEvent ( int x, int y );
     virtual void checkMousePressEvent ( int x, int y );
 
@@ -19,12 +42,22 @@ public:
                                agg::scanline_p8& sl, solidRenderer& solidRend ) const;
     virtual void paint ( base_renderer& baseRenderer ) const;
 
+protected slots:
+    void hideToolBar ();
+    void showToolBar ();
+    void changeToolBarPosition ();
+
 protected:
+    virtual void removeButtonHighlight ();
     virtual AggToolBarButton* getSelectedButton ( int x, int y ) const;
 
 private:
+    QPoint topPos;
+    bool enabled;
+    QTimer* timer;
     int cornerRadius;
-    color_type barFirstColor, barMiddleColor, barLastColor, selectionColor; 
+    color_type barFirstColor, barMiddleColor, barLastColor, selectionColor;
+    AggToolBarHideButton* hideButton;
 };
 
 #endif //TRUSSUNITTOOLBAR_H
