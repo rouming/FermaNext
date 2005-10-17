48df4cfb8ac4d8dd77bf436a0a50386e01185a6f
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2005-10-17 11:41:52 +0000

    Replace QMemArray with std::vector
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk/FermaNext@386 d51c77f7-620d-0410-a109-9a2f07eaa567

--- src/TrussUnit/TrussCalcData.h
+++ src/TrussUnit/TrussCalcData.h
@@ -2,27 +2,36 @@
 #ifndef TRUSSCALCDATA_H
 #define TRUSSCALCDATA_H
 
-#include <qmemarray.h>
+#include <vector>
 
 class TrussCalcData
 {
 public:
-    int nodesNum;                     //nodes number
-    int pivotsNum;                    //pivots number
-    int loadsNum;                     //load cases number
-    double v;                         //volume
-    double stressLimit;               //stress limit    
-    double forceWeight;               //truss force weight
-    QMemArray<int> pivotsFirstNodes;  //first nodes list of pivots
-    QMemArray<int> pivotsLastNodes;   //last nodes list of pivots
-    QMemArray<double> x;              //list of x nodes coord
-    QMemArray<double> y;              //list of y nodes coord
-    QMemArray<double> xTrans;         //list of nodes transference by x
-    QMemArray<double> yTrans;         //list of nodes transference by y
-    QMemArray<double> stress;         //list of pivots stresses
-    QMemArray<double> safetyFactor;   //list of pivots safety factor
-    QMemArray<double> pivotSquare;    //list of pivots square
-    QMemArray<double> pivotLength;    //list of pivots length
+    int nodesNum;                       //nodes number
+    int pivotsNum;                      //pivots number
+    int loadsNum;                       //load cases number
+    double v;                           //volume
+    double stressLimit;                 //stress limit    
+    double forceWeight;                 //truss force weight
+    std::vector<int> pivotsFirstNodes;  //first nodes list of pivots
+    std::vector<int> pivotsLastNodes;   //last nodes list of pivots
+    std::vector<double> x;              //list of x nodes coord
+    std::vector<double> y;              //list of y nodes coord
+    std::vector<double> xTrans;         //list of nodes transference by x
+    std::vector<double> yTrans;         //list of nodes transference by y
+    std::vector<double> stress;         //list of pivots stresses
+    std::vector<double> safetyFactor;   //list of pivots safety factor
+    std::vector<double> pivotSquare;    //list of pivots square
+    std::vector<double> pivotLength;    //list of pivots length
+
+    TrussCalcData () :
+        nodesNum(0),
+        pivotsNum(0),
+        loadsNum(0),
+        v(0),
+        stressLimit(0),
+        forceWeight(0)
+    {}
 };
 
 #endif //TRUSSCALCDATA_H
