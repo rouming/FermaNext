918c1c30f6d0a219068584a37c79acc66161a8f7
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2005-11-06 20:25:07 +0000

    3. Full project XML serialization. 2. Add string representation of format extension for    FermaNextProject, FermaNextWorkspace 3. Tweaks.
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk@436 d51c77f7-620d-0410-a109-9a2f07eaa567

--- FermaNext/src/FermaNext/FermaNextProject.cpp
+++ FermaNext/src/FermaNext/FermaNextProject.cpp
@@ -9,6 +9,10 @@
  * FermaNext Project
  *****************************************************************************/
 
+const QString FermaNextProject::FormatExtension = ".fpr";
+
+/*****************************************************************************/
+
 FermaNextProject::FermaNextProject ( const QString& name_, QWidgetStack* stack ) :
     name(name_),
     widgetStack(stack),
@@ -50,6 +54,57 @@ FermaNextProject::~FermaNextProject ()
         widgetStack->removeWidget( projectMainWidget );
     delete trussWindowManager;
     delete projectMainWidget;
+}
+
+QDomElement FermaNextProject::saveToXML ( QDomDocument& doc )
+{
+    QDomElement prjElem = XMLSerializableObject::saveToXML( doc );
+    prjElem.setTagName( "FermaNextProject" );
+
+    /**
+     * Save project name
+     ********************/
+    prjElem.setAttribute( "name", getName() );
+
+    /**
+     * Save truss unit windows
+     **************************/
+    // Save window stack
+    WindowList trussWindows = 
+        getDesignerWindow().getDesignerWidget().getTrussUnitWindowList();
+    WindowListIter wIter = trussWindows.begin();
+    for ( ; wIter != trussWindows.end(); ++wIter )
+        prjElem.appendChild( (*wIter)->saveToXML( doc ) );
+
+    return prjElem;
+}
+
+void FermaNextProject::loadFromXML ( const QDomElement& prjElem ) 
+    throw (LoadException)
+{
+    XMLSerializableObject::loadFromXML( prjElem );
+
+    /**
+     * Set project name
+     ********************/
+    if ( ! prjElem.hasAttribute( "name" ) )
+        throw LoadException();
+    setName( prjElem.attribute( "name" ) );
+
+
+    /**
+     * Create truss unit windows
+     ****************************/
+    TrussUnitWindowManager& mng = getTrussUnitWindowManager();
+    QDomNodeList trussWindows = prjElem.elementsByTagName( "TrussUnitWindow" );
+    for ( uint windsNum = 0; windsNum < trussWindows.count(); ++windsNum ) {
+        QDomNode trussWindow = trussWindows.item( windsNum );
+        if ( ! trussWindow.isElement() )
+            throw LoadException();
+        QDomElement windowElem = trussWindow.toElement();
+        TrussUnitWindow& wnd = mng.createTrussUnitWindow( QString::null );
+        wnd.loadFromXML( windowElem );
+    }
 }
 
 void FermaNextProject::activate ()
