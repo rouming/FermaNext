b2a8226c56cd264931c266ac338961fa7c82c34c
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2006-02-04 18:23:27 +0000

    Cover Windows behaviour code.
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk/FermaNext@502 d51c77f7-620d-0410-a109-9a2f07eaa567

--- tests/simple-tests/src/PathConvertionTest.cpp
+++ tests/simple-tests/src/PathConvertionTest.cpp
@@ -15,9 +15,10 @@ void my_assert( bool val, const QString& str )
 
 int main ()
 {
+    //FORMAT: file, basedir, result
     QString paths[][3] = 
     { 
-        // file, basedir, result
+        // original tests
         { "/some/path/dir/file", "/some/path/dir", "file" },
         { "/some/path/dir/file", "/some/path/dir/", "file" },
 
@@ -31,15 +32,37 @@ int main ()
 
         { "/some/path/dir/file", "/some/path/dir/another/dir", "../../file" },
         { "/some/path/dir/file", "/some/path/dir/another/", "../file" },
+
+        // different separators
+        { "\\some\\path/dir/file", "\\some/path",  "dir/file" },
+        { "/some/path\\dir\\file", "/some\\",  "path/dir/file" },
+        { "\\some/path/dir\\file", "\\",  "some/path/dir/file" },
+        { "\\some/path/dir/file", "",  "some/path/dir/file" },
+
+        // windows tests
+        { "C:\\some\\path\\dir\\file", "C:\\some\\path\\dir", "file" },
+        { "C:\\some\\path\\dir\\file", "C:\\some\\path\\dir\\", "file" },
+        { "C:\\some\\path\\dir\\file", "C:\\some\\path",  "dir/file" },
+        { "C:\\some\\path\\dir\\file", "C:\\some\\",  "path/dir/file" },
+        { "C:\\some\\path\\dir\\file", "C:\\some\\path\\dir\\another\\dir", 
+                                       "../../file" },
+        { "C:\\some\\path\\dir\\file", "C:\\some\\path\\dir\\another\\dir\\", 
+                                       "../../file" },
+
+        { "C:\\some\\path\\dir\\file", "D:\\some\\path\\dir", 
+                                       "C:/some/path/dir/file" },
+        { "C:\\some\\path\\dir\\file", "D:\\some\\path\\dir\\another\\dir", 
+                                       "C:/some/path/dir/file" },
     };
 
     for ( uint i = 0; i < sizeof(paths) / sizeof(paths[0]); ++i ) {
         QString rel = filePathToRelative( paths[i][0], paths[i][1] );
         QString abs = filePathToAbsolute( rel, paths[i][1] );
+        QString fileName = paths[i][0].replace('\\', '/');
         std::cout << "Test " << i+1 << "\n-------\n";
         my_assert( rel == paths[i][2], 
                    QString("Relative \"%1\"").arg(rel) );
-        my_assert( abs == paths[i][0], 
+        my_assert( abs == fileName, 
                    QString("Absolute \"%1\"").arg(abs) );
         std::cout << "\n";
     }
