275980c893e7d43203f40cdfd7797b97dfa60cae
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2006-02-07 16:01:33 +0000

    Remove dengerous and actually useless init/fini functions from plugin front-end
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk/FermaNext@513 d51c77f7-620d-0410-a109-9a2f07eaa567

--- src/common/plugin/NativePluginFrontEnd.h
+++ src/common/plugin/NativePluginFrontEnd.h
@@ -10,11 +10,7 @@
 #include "NativePlugin.h"
 
 /**
- * Define OS independent macros to provide init and fini functions
- * in dynamic libraries and plugin export macros.
- *
- * NOTE: init and fini macroses should always go after 
- * FERMA_NEXT_PLUGIN macros as in the example below.
+ * Define OS independent plugin export macros.
  *
  * Example:
  *
@@ -26,21 +22,10 @@
  * };
  * FERMA_NEXT_PLUGIN(SomeCalcPlugin);
  *
- * // init code
- * FERMA_NEXT_PLUGIN_INIT
- * {
- *   // some init code goes here
- * }
- *
- * // fini code
- * FERMA_NEXT_PLUGIN_FINI
- * {
- *   // some fini code goes here
- * }
  * @endcode
  */
 
-#define FERMA_NEXT_PLUGIN_HOOKS(PluginClass) \
+#define FERMA_NEXT_PLUGIN(PluginClass) \
     static PluginClass* plugin_instance__ = 0; \
     PluginExport NativePlugin* PLUGIN_INSTANCE_INIT ( PluginManager& mng,   \
                                                       const QString& path ) \
@@ -54,62 +39,5 @@
       delete plugin_instance__; \
       plugin_instance__ = 0; \
     }
-
-#if defined _WIN32 || defined WIN32
-  #include <windows.h>
-
-  #define FERMA_NEXT_PLUGIN_INIT \
-    void init_plugin__ (); \
-    class caller_init__ \
-    { public: caller_init__() { initCaller__ = init_plugin__; } }; \
-    caller_init__ init__; \
-    void init_plugin__ ()
-
-  #define FERMA_NEXT_PLUGIN_FINI \
-    void fini_plugin__ (); \
-    class caller_fini__ \
-    { public: caller_fini__() { finiCaller__ = fini_plugin__; } }; \
-    caller_fini__ fini__; \
-    void fini_plugin__ ()
-
-  #define FERMA_NEXT_PLUGIN(PluginClass) \
-    FERMA_NEXT_PLUGIN_HOOKS(PluginClass) \
-    \
-    typedef void (* InitFiniCall__) (); \
-    template <typename Call> \
-    class caller__ \
-    { \
-    public: \
-      caller__ () : call(0) {} \
-      caller__& operator= ( const Call& call_ ) \
-      { call = call_; return *this; } \
-      void operator() () { if ( call ) call(); } \
-    private: \
-      Call call; \
-    }; \
-    caller__<InitFiniCall__> initCaller__; \
-    caller__<InitFiniCall__> finiCaller__; \
-    \
-    BOOL WINAPI DllMain( \
-      HINSTANCE hinstDLL, \
-      DWORD fdwReason,    \
-      LPVOID lpvReserved  \
-    ) \
-    { \
-      if ( fdwReason == DLL_PROCESS_ATTACH ) \
-        initCaller__(); \
-      else if ( fdwReason == DLL_PROCESS_DETACH ) \
-        finiCaller__(); \
-      return TRUE; \
-    }
-#else
-  #define FERMA_NEXT_PLUGIN_INIT \
-    __attribute__((constructor)) void init_plugin__ ()
-  #define FERMA_NEXT_PLUGIN_FINI \
-    __attribute__((destructor)) void fini_plugin__ ()
-
-  #define FERMA_NEXT_PLUGIN(PluginClass) \
-    FERMA_NEXT_PLUGIN_HOOKS(PluginClass)
-#endif
 
 #endif //PLUGINFRONTEND_H
