d09c312d018a9b31dbd86b1a02176eb59798093a
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2006-02-09 22:59:20 +0000

    Tweaks around warning messages if JVM can't be started or doesn't exist
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk@516 d51c77f7-620d-0410-a109-9a2f07eaa567

--- FermaNext/src/loaders/JavaPluginLoader/JavaPluginLoader.cpp
+++ FermaNext/src/loaders/JavaPluginLoader/JavaPluginLoader.cpp
@@ -6,6 +6,10 @@
 
 #include "JavaVM/JavaVirtualMachine.h"
 
+#include <qmessagebox.h>
+#include <qfile.h>
+#include <stdlib.h>
+
 /*****************************************************************************
  * Plugin Loader hook
  *****************************************************************************/
@@ -18,18 +22,58 @@ JavaPluginLoader::JavaPluginLoader ( PluginManager& plgMng ) :
     PluginLoader( plgMng ),
     javaVM(0)
 {
-    try { 
-        QStringList options;
-        options.push_back("-verbose:jni");
-        javaVM = new JavaVirtualMachine(
-#if defined _WIN32 || defined WIN32  
-              "e:/java1.4.1/jre/bin/client/jvm.dll",
+    const char* envJavaHome = getenv("JAVA_HOME");
+    QString jvmLibPath;
+    QString jvmLibDir;
+    QString jvmLibi386Dir;
+    if ( envJavaHome == 0 ) {
+        QMessageBox::warning( 0, "Can't find JAVA_HOME environment", 
+                              "Please, specify JAVA_HOME environment\n"
+                              "if you want to use Java plugins\n\n"
+                              "Java loader has been disabled");
+        return;
+    } else {
+        const QString envJavaHomeStr( envJavaHome );
+#if defined _WIN32 || defined WIN32
+        jvmLibi386Dir = envJavaHomeStr + "/jre/bin/client";
+        jvmLibDir     = jvmLibi386Dir;
+        jvmLibPath    = jvmLibDir + "/jvm.dll";
 #else
-              "/home/roman/soft/j2sdk1.4.2_06/jre/lib/i386/client/libjvm.so",
+        jvmLibi386Dir = envJavaHomeStr + "/jre/lib/i386";
+        jvmLibDir     = jvmLibi386Dir + "/client";
+        jvmLibPath    = jvmLibDir + "/libjvm.so";
 #endif
-              JavaVirtualMachine::v1_4,
-              options );
-    } catch ( ... ) {}
+        if ( ! QFile::exists( jvmLibPath ) ) {
+            QMessageBox::warning( 0, "Can't find JVM lib",
+                           QString("JAVA_HOME environment is set to \"%1\"\n").
+                                  arg(envJavaHomeStr) + 
+                           QString("but can't find JVM lib there: \"%1\"\n\n").
+                                  arg(jvmLibPath) + 
+                           "Java loader has been disabled!" );
+            return;
+        }
+    }
+
+    QString envJavaHomeStr( envJavaHome );
+    QStringList options;
+    options.push_back("-verbose:jni");
+
+    try { javaVM = new JavaVirtualMachine( jvmLibPath,
+                                           JavaVirtualMachine::v1_4,
+                                           options ); } 
+    catch ( ... ) {        
+        QMessageBox::warning( 0, "Can't start JVM", 
+                              QString("JVM \"%1\" can't be started!\n").
+                                  arg(jvmLibPath)
+#if !defined _WIN32 && !defined WIN32
+                              + QString("Maybe you have forgot to specify "
+                                        "LD_LIBRARY_PATH?\nIt should be set "
+                                        "to\n  1. %1\n  2. %2\n").
+                                    arg(jvmLibi386Dir).arg(jvmLibDir)
+#endif
+                              + "\nJava loader has been disabled!"
+                              );        
+    }
 }
 
 JavaPluginLoader::~JavaPluginLoader ()
@@ -44,11 +88,11 @@ JavaPluginLoader::Status JavaPluginLoader::pluginLoaderStatusCode () const
 QString JavaPluginLoader::pluginLoaderStatusMsg () const
 { return QString(); }
 
-Plugin& JavaPluginLoader::specificLoadPlugin ( const QString& pathToPlugin )
+Plugin& JavaPluginLoader::specificLoadPlugin ( const QString& )
     throw (PluginLoadException)
 { return *((Plugin*)0); }
 
-void JavaPluginLoader::specificUnloadPlugin ( Plugin& plugin )
+void JavaPluginLoader::specificUnloadPlugin ( Plugin& )
 {}
 
 void JavaPluginLoader::specificUnloadPlugins ()
