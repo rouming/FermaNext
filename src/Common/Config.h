2182dc372b033ca336ad33869910d6c2fe315fe7
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2006-04-17 14:35:14 +0000

    Config class to manage xml FermaNext configuration
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk/FermaNext@576 d51c77f7-620d-0410-a109-9a2f07eaa567

---
+++ src/Common/Config.h
@@ -0,0 +1,170 @@
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <QObject>
+#include <QVariant>
+#include <QPair>
+#include <QList>
+#include <QDomElement>
+#include <QDomDocument>
+#include <QMutex>
+#include <QHash>
+#include <QFile>
+
+typedef QPair<QString, QString> NodeAttribute;
+typedef QList<NodeAttribute> NodeAttributeList;
+
+/**
+ * Configuration class, which manages xml configuration.
+ */
+class Config : public QObject
+{
+    Q_OBJECT
+public:
+    /**
+     * Inner class, which represents xml node of configuration.
+     */
+    class Node
+    {
+    public:
+        /** Constructs root configuration xml node  */
+        Node ( Config&, const QString& tagName );
+        /** Constructs configuration xml node with parent */
+        Node ( Node& parent, const QString& tagName );
+        
+        /** Just a copy constructor */
+        Node ( const Node& );
+        /** Just an assignmen operator */
+        Node& operator= ( const Node& );        
+
+        /** Returns config, from which this node was created */
+        const Config& config () const;
+        /** Returns config, from which this node was created */
+        Config& config ();
+
+        /** Returns parent node */
+        Node* parentNode () const;
+
+        /** Removes this node from config */
+        void remove ();
+
+        /** 
+         * Checks if this node was already removed 
+         * @return true if it is removed, false otherwise
+         */
+        bool isRemoved () const;
+
+        /** Return tag name of this node */
+        QString getTagName () const;
+        /** Sets tag name */
+        void setTagName ( const QString& );
+
+        /** Returns attributes of this node */
+        NodeAttributeList getAttributes () const;
+        /** Adds new node attribute */
+        bool addAttribute ( const NodeAttribute& );
+        /** Tries to remove attribute from node */
+        bool removeAttribute ( const QString& );
+        /** Clears all attributes */
+        void clearAttributes ();
+
+        /** Creates child node with specified tag name */
+        Node createChildNode ( const QString& tagName );
+        
+    private:
+        Config& cfg;
+        Node* parent;
+        QDomElement xmlData;
+        bool removedFlag;
+    };
+
+    // Inner Node class should be a friend of Config to have possibility 
+    // to notify directly about data changes
+    friend class Node;
+
+    //Exceptions
+    /** Can't open file for read/write */
+    class OpenFileException {};
+
+    /**
+     * Create an instance of config.
+     * @param fileName of configuration.
+     * @throw OpenFileException occurs when can't open file for read/write
+     */
+    static Config& instance ( const QString& fileName )
+        /*throw (OpenFileException)*/;
+
+    /** Returns root node of this config */
+    Config::Node rootNode () const;
+
+private:
+    /**
+     * Is called from inner Node class before making some changes. 
+     * After all changes have been made, node should unlock Config 
+     * instance with #nodeHasBeenChanged, #nodeHasBeenCreated or 
+     * #nodeBeforeRemove call.
+     */
+    void lockBeforeChange ();
+
+    /**
+     * Is called from inner Node class to notify about node changes.
+     * Should be called often after #lockBeforeChange.
+     */
+    void nodeHasBeenChanged ( const Config::Node& );
+
+    /**
+     * Is called from inner Node class to notify about node creation.
+     * Should be called often after #lockBeforeChange.
+     */
+    void nodeHasBeenCreated ( const Config::Node& );
+    
+    /**
+     * Is called from inner Node class to notify about node removal. 
+     * Should be called often after #lockBeforeChange.
+     */
+    void nodeHasBeenRemoved ( const Config::Node& );
+
+
+    /** Flushes all changes to file */
+    void flush ();
+
+    /** 
+     * Private constructor. Use #instance instead.
+     * @throw OpenFileException occurs when can't open file for read/write
+     */
+    Config ( const QString& fileName ) /*throw (OpenFileException)*/;
+    ~Config ();
+    Config ( const Config& );
+    Config& operator= ( const Config& );
+
+
+
+signals:
+    /** Emits, when some node has been changed */
+    void onNodeChanged ( const Config&, const Config::Node& );
+
+    /** Emits, when some node has been created */
+    void onNodeCreated ( const Config&, const Config::Node& );
+
+    /** Emits, when some node has been removed */
+    void onNodeRemoved ( const Config&, const Config::Node& );
+
+
+private:
+    typedef QHash<QString, Config*> HashInstances;
+
+    static HashInstances configInstances;
+    static QMutex instanceMutex;
+    static QMutex notificationMutex;
+
+    QDomDocument configDoc;
+    QFile configFile;
+    Config::Node* rootConfigNode;
+};
+
+typedef Config::Node ConfigNode;
+typedef QList<ConfigNode> ConfigNodeList;
+
+
+#endif //CONFIG_H
