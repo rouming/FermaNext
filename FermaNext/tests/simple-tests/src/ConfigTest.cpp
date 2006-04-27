1032be8d84131977768bd0e4e2dd07168b257299
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2006-04-27 20:58:49 +0000

    Have started implementing test for Config class
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk@589 d51c77f7-620d-0410-a109-9a2f07eaa567

--- FermaNext/tests/simple-tests/src/ConfigTest.cpp
+++ FermaNext/tests/simple-tests/src/ConfigTest.cpp
@@ -1,27 +1,97 @@
 
+#include <QFile>
+
 #include "Config.h"
 #include <iostream>
 
-int main ()
+const QString ConfigFileName( "../build/config.xml" );
+
+class ConfigTest
 {
-    Config& cfg = Config::instance("../build/config.xml");
-    ConfigNode root = cfg.rootNode();
-    std::cout << qPrintable( root.getTagName() ) << "\n";
+public:
+    ConfigTest () :
+        passed(0), failed(0)
+    {}
 
-    ConfigNodeList childs = root.childNodes();
-    foreach ( Config::Node node, childs ) {
-        std::cout << "\t" << qPrintable( node.getTagName() ) << "\n";
-        node.addAttribute( NodeAttribute("FF", "GGG") );
-        node.addAttribute( NodeAttribute("FF3333", "GGG") );
-
-        node.remove();
+    void my_assert ( bool val, const QString& str )
+    {
+        if ( val ) ++passed;
+        else ++failed;    
+        std::cout << (val ? "    OK: " : "NOT OK: ") << 
+                     qPrintable(str) << "\n"; 
     }
 
-    ConfigNode node = root.createChildNode("TrussUnit");
-    node.addAttribute( NodeAttribute("FF", "GGG") );
-    node = node.createChildNode("Color");
-    node.addAttribute( NodeAttribute("COlor", "GGG") );
+    void lastReport ()
+    {
+        std::cout << "\n=================>>>\n" << 
+                     "Passed: " << passed << "\n" << 
+                     "Failed: " << failed << "\n";
+    }
 
-    return 0;
+    void testCaseBegin ( const QString& testCaseName ) 
+    {
+        std::cout <<  "=========== " << qPrintable(testCaseName) << 
+                      " ==========\n";
+    }
+
+    void testCaseEnd () 
+    {
+        std::cout << "=================================\n\n";
+    }
+
+    uint status ()
+    { return failed; }
+
+
+    //////////////////////////////////////////////////////////////////////////
+    // TEST CASES
+    //////////////////////////////////////////////////////////////////////////
+
+    void configCreation ()
+    {
+        testCaseBegin("configCreation");
+
+        QFile::remove( ConfigFileName );
+        Config& config = Config::instance( ConfigFileName );
+        my_assert( QFile::exists(ConfigFileName), 
+                   "Config file should be created" );
+
+        Config& config2 = Config::instance( ConfigFileName );
+        my_assert( &config == &config2, "Instances should be unique" );
+                
+        testCaseEnd();
+    }
+
+
+private:
+    uint passed;
+    uint failed;
+};
+
+int main ()
+{
+    ConfigTest test;
+    test.configCreation();
+
+    /*
+
+    1. file creation
+    2. file parse (+ error)
+    3. create nodes (+ threads, + signal catch)
+    4. remove nodes (+ threads, + signal catch)
+    4. remove all child nodes (+ threads, + signal catch)
+    5. find nodes 
+    6. changing attributes (+ threads, + signal catch)
+    7. remove attributes by name (+ threads, + signal catch)
+    7. clearing all attributes (+ threads, + signal catch)
+    8. setting value  (+ threads, + signal catch, +checking child removal)
+    8. resetting value  (+ threads, + signal catch)
+    9. tag name changing (+ threads, + signal catch)
+
+    */
+
+    test.lastReport();
+   
+
+    return test.status();
 }
-
