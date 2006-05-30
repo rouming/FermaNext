f5cb5cd894e2cf1742ab90a3cd798ad6a49cc003
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2006-05-30 18:50:23 +0000

    Fix wrong log4cxx macros using
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk/FermaNext@631 d51c77f7-620d-0410-a109-9a2f07eaa567

--- src/Loaders/JavaPluginLoader/stubs-src/fermanext_logging_log4cxx_NDC.cpp
+++ src/Loaders/JavaPluginLoader/stubs-src/fermanext_logging_log4cxx_NDC.cpp
@@ -16,7 +16,7 @@ jstring JNICALL Java_fermanext_logging_log4cxx_NDC_get
   (JNIEnv* env, jclass)
 {
     std::basic_string<log4cxx::logchar> ndcStr = NDC::pop();
-#ifdef LOG4CXX_LOGCHAR_IS_UTF8
+#if LOG4CXX_LOGCHAR_IS_UTF8
     return env->NewStringUTF( ndcStr.data() );
 #else
     return env->NewString( ndcStr.data(), ndcStr.length() );
@@ -32,7 +32,7 @@ jstring JNICALL Java_fermanext_logging_log4cxx_NDC_pop
   (JNIEnv* env, jclass)
 {
     std::basic_string<log4cxx::logchar> ndcStr = NDC::pop();
-#ifdef LOG4CXX_LOGCHAR_IS_UTF8
+#if LOG4CXX_LOGCHAR_IS_UTF8
     return env->NewStringUTF( ndcStr.data() );
 #else
     return env->NewString( ndcStr.data(), ndcStr.length() );
