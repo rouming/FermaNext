23831635e71190ed1c30d15a8975ac5cb15d3b56
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2006-06-09 07:16:23 +0000

    Add logging output to native plugin loader
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk/FermaNext@643 d51c77f7-620d-0410-a109-9a2f07eaa567

--- src/Loaders/NativePluginLoader/NativePluginLoader.cpp
+++ src/Loaders/NativePluginLoader/NativePluginLoader.cpp
@@ -5,6 +5,7 @@
 #include "PluginManager.h"
 #include "DynaLoader.h"
 #include "Plugin.h"
+#include "Log4CXX.h"
 
 /*****************************************************************************
  * Plugin Loader hook
@@ -15,6 +16,14 @@
  * to be often loaded before others loaders.
  */
 FERMA_NEXT_PLUGIN_LOADER(NativePluginLoader, (PluginManager::LoadingPriority)0)
+
+/*****************************************************************************
+ * Logger
+ *****************************************************************************/
+
+using log4cxx::LoggerPtr;
+using log4cxx::Logger;
+static LoggerPtr logger( Logger::getLogger("loader.NativePluginLoader") );
 
 /*****************************************************************************
  * Native Plugin Loader
@@ -46,6 +55,9 @@ QString NativePluginLoader::pluginLoaderStatusMsg () const
 Plugin& NativePluginLoader::specificLoadPlugin ( const QString& pathToPlugin )
     /*throw (PluginLoadException)*/
 {
+    LOG4CXX_DEBUG(logger, "specificLoadPlugin(QString): " + 
+                  pathToPlugin.toStdString() );
+
     DynaLoader* dynaLoader = new DynaLoader;
     NativePlugin* plugin = 0;
     try {
@@ -55,36 +67,53 @@ Plugin& NativePluginLoader::specificLoadPlugin ( const QString& pathToPlugin )
                                                    PLUGIN_INSTANCE_INIT_CALL );
         plugin = pluginInstanceInitCall( pluginManager(), pathToPlugin );
     }
-    catch ( DynaLoader::LibraryLoadException& ) {
+    catch ( DynaLoader::LibraryLoadException& e ) {
+        LOG4CXX_WARN(logger, "plugins can't be loaded: " + 
+                     pathToPlugin.toStdString() + ", can't load library: "+ 
+                     e.message().toStdString() );
         delete dynaLoader;
-        // TODO: log this exception
         throw PluginLoadException();
     }
-    catch ( DynaLoader::AddressException& ) {
+    catch ( DynaLoader::AddressException& e ) {
+        LOG4CXX_WARN(logger, "plugin can't be loaded: " + 
+                     pathToPlugin.toStdString() + ", can't find address: "+
+                     e.message().toStdString() );
         delete dynaLoader;
-        // TODO: log this exception
         throw PluginLoadException();
     }
     catch ( ... ) {
-        // Unknown plugin exception
-        // TODO: log this exception
+        LOG4CXX_ERROR(logger, "plugin can't be loaded: " + 
+                      pathToPlugin.toStdString() + ", unknown exception");
         throw PluginLoadException();
     }
 
     if ( plugin == 0 ) {
+        LOG4CXX_WARN(logger, "plugins can't be loaded: " + 
+                     pathToPlugin.toStdString() + ", loader pointer is 0");
+
         delete dynaLoader;
         throw PluginLoadException();
     }
+
+    LOG4CXX_INFO(logger, "plugin loaded: " +  pathToPlugin.toStdString());
+
     pluginMap[plugin] = dynaLoader;
     return *plugin;
 }
 
 void NativePluginLoader::specificUnloadPlugin ( Plugin& plugin )
 {
+    QString pluginPath = plugin.pluginPath();
+    LOG4CXX_DEBUG(logger, "specificUnloadPlugin: " +
+                  pluginPath.toStdString());
+
     try { 
         NativePlugin& nativePlg = dynamic_cast<NativePlugin&>( plugin );
-        if ( ! pluginMap.contains( &nativePlg ) )
+        if ( ! pluginMap.contains( &nativePlg ) ) {
+            LOG4CXX_WARN(logger, "plugin doesn't exist: " +
+                          pluginPath.toStdString());            
             return;
+        }
 
         DynaLoader* dynaLoader = pluginMap[&nativePlg];
         pluginMap.remove( &nativePlg );
@@ -94,19 +123,25 @@ void NativePluginLoader::specificUnloadPlugin ( Plugin& plugin )
                                                    PLUGIN_INSTANCE_FINI_CALL );
             pluginInstanceFiniCall();
         }
-        catch ( DynaLoader::AddressException& ) {
-            // TODO: log this exception            
-        }
+        catch ( DynaLoader::AddressException& e ) {
+            LOG4CXX_WARN(logger, "plugin can't be correctly unloaded: " + 
+                         pluginPath.toStdString() + ", can't find address: "+
+                         e.message().toStdString() );
+        }       
 
         delete dynaLoader;
+
+        LOG4CXX_INFO(logger, "plugin unloaded: " +  pluginPath.toStdString());
     }
     catch ( std::exception& ) {
-        // cast exception
-        //TODO: log this exception
+        // Cast exception
+        LOG4CXX_WARN(logger, "plugin can't be correctly unloaded: " + 
+                     pluginPath.toStdString() + 
+                     ", is not a child of base plugin class");
     }
     catch ( ... ) {
-        // Unknown plugin exception
-        //TODO: log this exception
+        LOG4CXX_ERROR(logger, "plugin can't be loaded: " + 
+                      pluginPath.toStdString() + ", unknown exception");
     }
 }
 
