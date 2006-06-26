873ed9bb79707846b4522b3d5a49640d837f89cc
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2006-06-26 12:13:23 +0000

    Continue implementing Java plugins framework
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk/FermaNext@655 d51c77f7-620d-0410-a109-9a2f07eaa567

---
+++ src/Loaders/JavaPluginLoader/java-src/fermanext/system/NativePlugin.java
@@ -0,0 +1,56 @@
+
+package fermanext.system;
+
+import java.util.UUID;
+
+public class NativePlugin extends Plugin 
+                          implements NativeObject
+{
+    public NativePlugin ( String pluginPath )
+    { super(pluginPath); }
+
+    /**
+     * This is a main execute method.
+     */
+    public native void execute ( UUIDObject[] uuids );
+
+    /** 
+     * Describes itself.
+     * @return plugin info
+     */
+    public native PluginInfo pluginInfo ();
+
+    /** 
+     * Describes plugin state by status code.
+     * @return plugin status code
+     * @see pluginStatusMsg for detailed information about status.
+     */
+    public native Status pluginStatusCode ();
+
+    /** 
+     * Describes plugin state by status msg. 
+     * (e.g. describes the sort of internal error if it has been occured)
+     * @return plugin status str msg. Returns empty string if status is Ok
+     * @see pluginStatusCode
+     */
+    public native String pluginStatusMsg ();
+
+
+    // Random UUID
+    private String uuid = UUID.randomUUID().toString();
+
+    // UUID getter.
+    public final String getUUID ()
+    { return uuid; }
+
+    // UUID setter.
+    public final void setUUID ( String uuid )
+    { this.uuid = uuid; }
+
+    // Checks if this plugin really wraps native class
+    public native boolean isValid ();
+
+    static { 
+        System.loadLibrary("JavaPluginLoader.ldr");
+    }
+}
---
+++ src/Loaders/JavaPluginLoader/java-src/fermanext/system/UUIDObject.java
@@ -0,0 +1,19 @@
+
+package fermanext.system;
+
+import java.util.UUID;
+
+public interface UUIDObject
+{
+    /**
+     * UUID getter.
+     * @return uuid
+     */
+    public String getUUID ();
+
+    /**
+     * UUID setter.
+     * @param uuid of native object
+     */
+    public void setUUID ( String uuid );
+}
