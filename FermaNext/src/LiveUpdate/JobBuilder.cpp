6592c068153af567072491f95e0e8ab2221cc262
rouming <rouming@d51c77f7-620d-0410-a109-9a2f07eaa567>
2007-03-07 14:07:39 +0000

    Continue implementing LiveUpdate
    git-svn-id: https://ferma.svn.sf.net/svnroot/ferma/trunk@802 d51c77f7-620d-0410-a109-9a2f07eaa567

--- FermaNext/src/LiveUpdate/JobBuilder.cpp
+++ FermaNext/src/LiveUpdate/JobBuilder.cpp
@@ -1,5 +1,6 @@
 
 #include <QRegExp>
+#include <QUuid>
 
 #include "JobBuilder.h"
 #include "Log4CXX.h"
@@ -12,9 +13,81 @@ using log4cxx::LoggerPtr;
 using log4cxx::Logger;
 static LoggerPtr logger( Logger::getLogger("LiveUpdate.JobBuilder") );
 
+/*****************************************************************************
+ * Helper
+ *****************************************************************************/
+
+#define DO_GET "get"
+#define DO_REPLACE "replace"
+#define DO_DELETE "delete"
+#define CONFLICT "conflict"
+
+bool SortElements ( const QDomElement& fir,  const QDomElement& sec )
+{
+    QString firSortString;
+    QString secSortString;
+
+    QString firStatus = fir.attribute("status");
+    QString secStatus = sec.attribute("status");
+
+    // First status
+    if ( firStatus.contains(DO_GET) )
+        firSortString += "A";
+    else if ( firStatus.contains(DO_REPLACE) )
+        firSortString += "B";
+    else if ( firStatus.contains(DO_DELETE) )
+        firSortString += "C";
+
+    // Second status
+    if ( secStatus.contains(DO_GET) )
+        secSortString += "A";
+    else if ( secStatus.contains(DO_REPLACE) )
+        secSortString += "B";
+    else if ( secStatus.contains(DO_DELETE) )
+        secSortString += "C";
+
+
+    QString firTag = fir.tagName();
+    QString secTag = sec.tagName();
+
+    // First tag
+    if ( firTag == "Directory" ) {
+        if ( firStatus.contains(DO_DELETE) )
+            firSortString += "B";
+        else
+            firSortString += "A";
+    }
+    else if ( firTag.contains("File") ) {
+        if ( firStatus.contains(DO_DELETE) )
+            firSortString += "A";
+        else
+            firSortString += "B";
+    }
+
+    // Second tag
+    if ( secTag == "Directory" ) {
+        if ( secStatus.contains(DO_DELETE) )
+            secSortString += "B";
+        else
+            secSortString += "A";
+    }
+    else if ( secTag.contains("File") ) {
+        if ( secStatus.contains(DO_DELETE) )
+            secSortString += "A";
+        else
+            secSortString += "B";
+    }
+
+    firSortString = fir.attribute("name");
+    secSortString = sec.attribute("name");
+
+    return firSortString < secSortString;
+}
+
 /*****************************************************************************/
 
 Job::Job ( Job::JobType jobType, bool isConflict ) :
+    m_jobUuid( QUuid::createUuid().toString() ),
     m_jobType( jobType ),
     m_isConflict( isConflict )
 {}
@@ -27,6 +100,9 @@ bool Job::isConflict () const
 
 void Job::resolveConflict ()
 { m_isConflict = false; }
+
+const QString& Job::jobUuid () const
+{ return m_jobUuid; }
 
 Job::JobType Job::jobType () const
 { return m_jobType; }
@@ -173,82 +249,29 @@ bool CreateDirJob::undoJob ()
 
 /*****************************************************************************/
 
-#define DO_GET "get"
-#define DO_REPLACE "replace"
-#define DO_DELETE "delete"
-#define CONFLICT "conflict"
-
-bool SortElements ( const QDomElement& fir,  const QDomElement& sec )
-{
-    QString firSortString;
-    QString secSortString;
-
-    QString firStatus = fir.attribute("status");
-    QString secStatus = sec.attribute("status");
-
-    // First status
-    if ( firStatus.contains(DO_GET) )
-        firSortString += "A";
-    else if ( firStatus.contains(DO_REPLACE) )
-        firSortString += "B";
-    else if ( firStatus.contains(DO_DELETE) )
-        firSortString += "C";
-
-    // Second status
-    if ( secStatus.contains(DO_GET) )
-        secSortString += "A";
-    else if ( secStatus.contains(DO_REPLACE) )
-        secSortString += "B";
-    else if ( secStatus.contains(DO_DELETE) )
-        secSortString += "C";
-
-
-    QString firTag = fir.tagName();
-    QString secTag = sec.tagName();
-
-    // First tag
-    if ( firTag == "Directory" ) {
-        if ( firStatus.contains(DO_DELETE) )
-            firSortString += "B";
-        else
-            firSortString += "A";
-    }
-    else if ( firTag.contains("File") ) {
-        if ( firStatus.contains(DO_DELETE) )
-            firSortString += "A";
-        else
-            firSortString += "B";
-    }
-
-    // Second tag
-    if ( secTag == "Directory" ) {
-        if ( secStatus.contains(DO_DELETE) )
-            secSortString += "B";
-        else
-            secSortString += "A";
-    }
-    else if ( secTag.contains("File") ) {
-        if ( secStatus.contains(DO_DELETE) )
-            secSortString += "A";
-        else
-            secSortString += "B";
-    }
-
-    firSortString = fir.attribute("name");
-    secSortString = sec.attribute("name");
-
-    return firSortString < secSortString;
-}
-
-/*****************************************************************************/
-
-JobBuilder::JobBuilder ( const QDomDocument& md5Compared )
+JobBuilder::JobBuilder ( const QDomDocument& md5Compared ) :
+    m_currentJob(0)
 {
     QDomElement md5CmpElem = md5Compared.documentElement();
     QList<QDomElement> elems;
     parseDocument( md5CmpElem, elems );
     qSort( elems.begin(), elems.end(), SortElements );
     createJobsList( elems );
+}
+
+JobBuilder::~JobBuilder ()
+{
+    clearJobs();
+}
+
+void JobBuilder::clearJobs ()
+{
+    m_currentJob = 0;
+
+    QList<Job*>::Iterator it = m_jobs.begin();
+    for ( ; it != m_jobs.end(); ++it )
+        delete *it;
+    m_jobs.clear();
 }
 
 void JobBuilder::parseDocument ( const QDomElement& md5CmpElem,
@@ -282,6 +305,11 @@ void JobBuilder::createJobsList ( const QList<QDomElement>& elements )
     QRegExp conflictRegExp("^" + QString(CONFLICT) + "\\(.+\\)$");
 
     QList<QDomElement>::ConstIterator it = elements.begin();
+
+    QList<Job*> firstSteps;
+    QList<Job*> secondSteps;
+    QList<Job*> thirdSteps;
+
     for ( ; it != elements.end(); ++it ) {
         QDomElement elem = *it;
 
@@ -290,37 +318,118 @@ void JobBuilder::createJobsList ( const QList<QDomElement>& elements )
 
             QString status = elem.attribute("status");
 
-            bool isConflict = conflictRegExp.exactMatch(status);
+            bool conflict = conflictRegExp.exactMatch(status);
         
-            Job* job = 0;
-
-            if ( status.contains(DO_REPLACE) ) {
+            if ( status.contains(DO_GET) ) {
+                firstSteps.append(
+                          new DownloadJob( "url", "path_to_save", conflict ) );
+            }
+            else if ( status.contains(DO_REPLACE) ) {
+                firstSteps.append(
+                          new DownloadJob( "url", "path_to_save", conflict ) );
+                secondSteps.append(
+                          new RenameJob( "from", "to", conflict ) );
+                thirdSteps.append(
+                          new DeleteJob( "path_to_delete", conflict ) );
             }
             else if ( status.contains(DO_DELETE) ) {
-            }
-            else if ( status.contains(DO_GET) ) {
+                secondSteps.append( 
+                          new RenameJob( "from", "to", conflict ) );
+                thirdSteps.append( 
+                          new DeleteJob("path_to_delete", conflict ) );
             }
             else 
                 // Ok, status is unknown
-                return;
-
-            m_jobs.append(job);
+                continue;
         }
+        else if ( elem.tagName() == "Directory" &&
+                  elem.hasAttribute("status") ) {
 
+            QString status = elem.attribute("status");
+
+            bool conflict = conflictRegExp.exactMatch(status);
+        
+            if ( status.contains(DO_GET) ) {
+                firstSteps.append(
+                          new CreateDirJob( "path_to_create", conflict ) );
+            }
+            else if ( status.contains(DO_REPLACE) ) {
+                // Never should happen for directory
+                continue;
+            }
+            else if ( status.contains(DO_DELETE) ) {
+                thirdSteps.append(
+                          new DeleteJob( "path_to_delete", conflict ) );
+            }
+            else 
+                // Ok, status is unknown
+                continue;
+        }
+        else 
+            // Unknown element
+            continue;
     }
+
+    // Firstly do clean
+    clearJobs();
+
+    // Append in order
+    m_jobs = firstSteps + secondSteps + thirdSteps;
 }
 
 void JobBuilder::doJobs ()
 {
-    //TODO
+    QList<Job*>::Iterator it;
+
+    double percentsPerJob = 100 / m_jobs.size();
+    
+    emit beforeDoJobs( m_jobs.size() );
+
+    for ( it = m_jobs.begin(); it != m_jobs.end(); ++it ) {
+        Job* job = *it;
+        bool jobRes = job->doJob();
+        if ( ! jobRes ) {
+            m_currentJob = job;
+            emit jobFailed( job->jobUuid() );
+            return;
+        }
+    }
 }
 
 void JobBuilder::undoJobs ()
 {
-    //TODO
+    if ( m_currentJob == 0 )
+        // Nothing to undo
+        return;
+
+    int index = m_jobs.indexOf( m_currentJob );
+
+    // If nothing was found
+    if ( index == -1 ) {
+        m_currentJob = 0;
+        return;
+    }
+
+    emit beforeUndoJobs( index + 1 );
+
+    for ( int i = index; i >= 0; --i ) {
+        Job* job = m_jobs[i];
+        job->undoJob();
+    }
+
+    m_currentJob = 0;
 }
 
 const QList<Job*>& JobBuilder::getJobs () const
 { return m_jobs; }
+
+Job* JobBuilder::findJobByUuid ( const QString& uuid ) const
+{
+    QList<Job*>::ConstIterator it;
+    for ( it = m_jobs.begin(); it != m_jobs.end(); ++it )
+        if ( (*it)->jobUuid() == uuid )
+            return *it;
+    return 0;
+}
 
 /*****************************************************************************/
